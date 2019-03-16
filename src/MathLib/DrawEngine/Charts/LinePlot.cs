b1e52afb983ec75468bfd2e464e3abbd29884ae7
Vitaliy Dobriyan <dobriy88@yandex.ru>
2019-03-17 00:01:51 +0400

    Common Scatter and Line plots code moved to DataSeriesPlot

--- src/MathLib/DrawEngine/Charts/LinePlot.cs
+++ src/MathLib/DrawEngine/Charts/LinePlot.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
 using MathLib.Data;
 
 namespace MathLib.DrawEngine.Charts
@@ -10,90 +8,24 @@ namespace MathLib.DrawEngine.Charts
     /// <summary>
     /// Class for Multi Signal plot
     /// </summary>
-    public class LinePlot : PlotObject
+    public class LinePlot : DataSeriesPlot
     {
-        private DataPoint tsPointMax;
-        private DataPoint tsPointMin;
-        private DataPoint tsAmplitude;
-
         public LinePlot(Size bitmapSize) 
-            : base(bitmapSize, 1f)
+            : base(bitmapSize)
         {
-            this.TimeSeriesList = new List<Timeseries>();
-            this.PlotPens = new List<Pen>();
-            this.tsAmplitude = new DataPoint(0, 0);
-            this.tsPointMax = new DataPoint(double.MinValue, double.MinValue);
-            this.tsPointMin = new DataPoint(double.MaxValue, double.MaxValue);
         }
 
         public LinePlot(Size bitmapSize, Timeseries dataSeries, Color color, float thickness) 
-            : this(bitmapSize)
+            : base(bitmapSize, dataSeries, color, thickness)
         {
-            this.AddDataSeries(dataSeries, color, thickness);
         }
 
         public LinePlot(Size bitmapSize, Timeseries dataSeries) 
-            : this(bitmapSize, dataSeries, Color.SteelBlue, 1f)
+            : base(bitmapSize, dataSeries)
         {
         }
 
-        protected List<Timeseries> TimeSeriesList { get; set; }
-
-        protected List<Pen> PlotPens { get; set; }
-
-        public void AddDataSeries(Timeseries dataSeries, Color color, float thickness)
-        {
-            this.TimeSeriesList.Add(dataSeries);
-            this.PlotPens.Add(new Pen(color, thickness));
-
-            this.tsPointMax.X = Math.Max(this.tsPointMax.X, dataSeries.Max.X);
-            this.tsPointMax.Y = Math.Max(this.tsPointMax.Y, dataSeries.Max.Y);
-            this.tsPointMin.X = Math.Min(this.tsPointMin.X, dataSeries.Min.X);
-            this.tsPointMin.Y = Math.Min(this.tsPointMin.Y, dataSeries.Min.Y);
-
-            this.tsAmplitude.X = this.tsPointMax.X - this.tsPointMin.X;
-            this.tsAmplitude.Y = this.tsPointMax.Y - this.tsPointMin.Y;
-        }
-
-        public void AddDataSeries(Timeseries dataSeries, Color color) =>
-            AddDataSeries(dataSeries, color, 1f);
-
-        public override Bitmap Plot()
-        {
-            PrepareChartArea();
-
-            if (this.TimeSeriesList.All(ts => ts.Length < 1))
-            {
-                NoDataToPlot();
-            }
-            else
-            {
-                CalculateChartAreaSize(this.tsAmplitude);
-
-                for (int i = 0; i < this.TimeSeriesList.Count; i++)
-                {
-                    DrawDataSeries(this.TimeSeriesList[i], this.PlotPens[i]);
-                }
-
-                DrawGrid();
-            }
-
-            g.Dispose();
-
-            return PlotBitmap;
-        }
-
-        protected override void DrawGrid()
-        {
-            SetAxisValues(
-                GetAxisValue(this.tsPointMin.X),
-                GetAxisValue(this.tsPointMax.X),
-                GetAxisValue(this.tsPointMin.Y),
-                GetAxisValue(this.tsPointMax.Y)
-            );
-        }
-
-        private void DrawDataSeries(Timeseries ds, Pen pen)
+        protected override void DrawDataSeries(Timeseries ds, Pen pen)
         {
             double xPl, yPl;
             var points = new List<PointF>();
--- src/MathLib/DrawEngine/Charts/ScatterPlot.cs
+++ src/MathLib/DrawEngine/Charts/ScatterPlot.cs
@@ -1,97 +1,29 @@
 ﻿using MathLib.Data;
 using System.Drawing;
-using System.Collections.Generic;
-using System;
-using System.Linq;
 
 namespace MathLib.DrawEngine.Charts
 {
     /// <summary>
     /// Class for Poincare map
     /// </summary>
-    public class ScatterPlot : PlotObject
+    public class ScatterPlot : DataSeriesPlot
     {
-        private DataPoint tsPointMax;
-        private DataPoint tsPointMin;
-        private DataPoint tsAmplitude;
-
-        protected List<Timeseries> TimeSeriesList;
-        protected List<Pen> PlotPens;
-
         public ScatterPlot(Size pictureboxSize)
-            : base(pictureboxSize, 1f)
+            : base(pictureboxSize)
         {
-            this.TimeSeriesList = new List<Timeseries>();
-            this.PlotPens = new List<Pen>();
-            this.tsAmplitude = new DataPoint(0, 0);
-            this.tsPointMax = new DataPoint(double.MinValue, double.MinValue);
-            this.tsPointMin = new DataPoint(double.MaxValue, double.MaxValue);
         }
 
         public ScatterPlot(Size bitmapSize, Timeseries dataSeries, Color color, float thickness) 
-            : this(bitmapSize)
+            : base(bitmapSize, dataSeries, color, thickness)
         {
-            this.AddDataSeries(dataSeries, color, thickness);
         }
 
         public ScatterPlot(Size bitmapSize, Timeseries dataSeries) 
-            : this(bitmapSize, dataSeries, Color.SteelBlue, 1f)
+            : base(bitmapSize, dataSeries)
         {
         }
 
-        public void AddDataSeries(Timeseries dataSeries, Color color, float thickness)
-        {
-            this.TimeSeriesList.Add(dataSeries);
-            this.PlotPens.Add(new Pen(color, thickness));
-
-            this.tsPointMax.X = Math.Max(this.tsPointMax.X, dataSeries.Max.X);
-            this.tsPointMax.Y = Math.Max(this.tsPointMax.Y, dataSeries.Max.Y);
-            this.tsPointMin.X = Math.Min(this.tsPointMin.X, dataSeries.Min.X);
-            this.tsPointMin.Y = Math.Min(this.tsPointMin.Y, dataSeries.Min.Y);
-
-            this.tsAmplitude.X = this.tsPointMax.X - this.tsPointMin.X;
-            this.tsAmplitude.Y = this.tsPointMax.Y - this.tsPointMin.Y;
-        }
-
-        public void AddDataSeries(Timeseries dataSeries, Color color) =>
-            AddDataSeries(dataSeries, color, 1f);
-
-        public override Bitmap Plot()
-        {
-            PrepareChartArea();
-
-            if (this.TimeSeriesList.All(ts => ts.Length < 1))
-            {
-                NoDataToPlot();
-            }
-            else
-            {
-                CalculateChartAreaSize(this.tsAmplitude);
-
-                for (int i = 0; i < this.TimeSeriesList.Count; i++)
-                {
-                    DrawDataSeries(this.TimeSeriesList[i], this.PlotPens[i]);
-                }
-
-                DrawGrid();
-            }
-            
-            g.Dispose();
-
-            return PlotBitmap;
-        }
-
-        protected override void DrawGrid()
-        {
-            SetAxisValues(
-                GetAxisValue(this.tsPointMin.X),
-                GetAxisValue(this.tsPointMax.X),
-                GetAxisValue(this.tsPointMin.Y),
-                GetAxisValue(this.tsPointMax.Y)
-            );
-        }
-
-        private void DrawDataSeries(Timeseries ds, Pen pen)
+        protected override void DrawDataSeries(Timeseries ds, Pen pen)
         {
             double xPl, yPl;
 
