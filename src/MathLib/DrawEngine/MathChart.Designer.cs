fd66cb0717bc0740a7516398413bf2ff9acef80e
Vitaliy Dobriyan <dobriy88@yandex.ru>
2019-05-12 20:07:01 +0400

    made specified Charts realization and applied to all charts in TSA

---
+++ src/MathLib/DrawEngine/MathChart.Designer.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MathLib.DrawEngine
+{
+    partial class MathChart
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            //this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+
+            var chartArea = new ChartArea();
+            chartArea.Name = "ChartArea";
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisX.IsLabelAutoFit = true;
+            chartArea.AxisX.LabelStyle.Format = "G5";
+            chartArea.AxisX.LabelStyle.TruncatedLabels = true;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsLabelAutoFit = true;
+            //schartArea.AxisY.IntervalAutoMode = IntervalAutoMode.VariableCount;
+            //chartArea.AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+
+            chartArea.AxisX.LabelAutoFitStyle = 
+                LabelAutoFitStyles.IncreaseFont |
+                LabelAutoFitStyles.DecreaseFont |
+                LabelAutoFitStyles.StaggeredLabels;
+
+            chartArea.AxisY.LabelAutoFitStyle = 
+                LabelAutoFitStyles.IncreaseFont |
+                LabelAutoFitStyles.DecreaseFont |
+                LabelAutoFitStyles.StaggeredLabels;
+            
+            this.ChartAreas.Add(chartArea);
+
+            this.Cursor = Cursors.Cross;
+            this.Palette = ChartColorPalette.SeaGreen;
+        }
+
+        #endregion
+    }
+}
---
+++ src/MathLib/DrawEngine/MathChart.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathLib.Data;
+
+namespace MathLib.DrawEngine
+{
+    public partial class MathChart : Chart
+    {
+        public MathChart()
+        {
+            InitializeComponent();
+        }
+
+        public bool HasData => this.Series.Any(s => s.Points.Any());
+
+        //chart.ChartAreas[0].Axes[0].Title = "t";
+        //        chart.ChartAreas[0].Axes[1].Title = "Slope";
+
+        public void AddTimeSeries(string seriesName, Timeseries timeseries, SeriesChartType type, Color color, int markerSize)
+        {
+            var series = new Series();
+            series.ChartArea = "ChartArea";
+            series.ChartType = type;
+            series.Color = color;
+            series.MarkerColor = color;
+            series.MarkerSize = markerSize;
+            series.Name = seriesName;
+
+            foreach (var dp in timeseries.DataPoints)
+            {
+                series.Points.AddXY(dp.X, dp.Y);
+            }
+
+            this.Series.Add(series);
+
+            var newInterval = Math.Max(this.ChartAreas[0].AxisX.Interval, timeseries.Amplitude.X);
+            this.ChartAreas[0].AxisX.Interval = newInterval;
+            this.ChartAreas[0].RecalculateAxesScale();
+            this.Update();
+        }
+
+        public void AddTimeSeries(string seriesName, Timeseries timeseries, SeriesChartType type, Color color) =>
+            AddTimeSeries(seriesName, timeseries, type, color, 2);
+
+        public void AddTimeSeries(string seriesName, Timeseries timeseries, SeriesChartType type) =>
+            AddTimeSeries(seriesName, timeseries, type, Color.SteelBlue, 2);
+
+        public void ClearChart() =>
+            this.Series.Clear();
+    }
+}
