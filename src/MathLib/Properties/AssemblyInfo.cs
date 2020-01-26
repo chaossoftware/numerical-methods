09d0256cdf1120824f415707a9e820508a434ddf
Vitaliy Dobriyan <dobriy88@yandex.ru>
2020-01-26 23:35:24 +0400

    added nuspec for MathLib

--- src/MathLib/Properties/AssemblyInfo.cs
+++ src/MathLib/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MathLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Math")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("EPAM Systems")]
 [assembly: AssemblyProduct("MathLib")]
