c046e9f9c54796672a62ed1d2e8769677206e576
Vitaliy Dobriyan <dobriy88@yandex.ru>
2022-08-01 19:01:49 +0300

    toolbox: reincarnated matlab fft and wavelets

---
+++ src/ChaosSoft.Core/Transform/Wavelet.cs
@@ -0,0 +1,21 @@
+﻿using MathWorks.MATLAB.NET.Arrays;
+
+namespace ChaosSoft.Core.Transform
+{
+    public class Wavelet
+    {
+        public static void BuildWavelet(double[] timeSeries, string tmpFileName, string wName, 
+            double tStart, double tEnd, double fStart, double fEnd, double dt, string colMap, double width, double height)
+        {
+            var matlabEngine = new MatlabEngine.MatlabEngine();
+            var mwFolder = new MWCharArray(string.Empty);
+            var mwfileName = new MWCharArray(tmpFileName);
+            var mwWname = new MWCharArray(wName);
+            var mwColMap = new MWCharArray(colMap);
+            var mwSignalArray = new MWNumericArray(timeSeries);
+
+            matlabEngine.Get2DWavelet(
+                mwSignalArray, mwFolder, mwfileName, mwWname, tStart, tEnd, fStart, fEnd, dt, mwColMap, width, height);
+        }
+    }
+}
